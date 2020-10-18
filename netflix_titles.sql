ba935ea09becb9c8a26ecccba4728f095e32d113
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-10-18 09:10:39 +0700

    Add files via upload

---
+++ netflix_titles.sql
@@ -0,0 +1,26 @@
+CREATE TABLE netflix_titles (
+show_id INT(11) PRIMARY KEY,
+type1 VARCHAR(10),
+title VARCHAR(70),
+director VARCHAR(30),
+cast1 VARCHAR(30),
+country VARCHAR(30),
+date_added DATE,
+release_year YEAR,
+rating VARCHAR(20),
+duration VARCHAR(20),
+listed_in VARCHAR(30),
+description LONGTEXT
+);
+
+DESCRIBE netflix_titles
+
+
+LOAD DATA INFILE 'D:/BKPSDM/netflix_titles.csv'
+INTO TABLE netflix_titles
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+SELECT * FROM netflix_titles
